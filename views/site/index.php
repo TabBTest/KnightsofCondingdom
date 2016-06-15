18d8f2234a061f7ba8c874e4642f9ac53813cd67
Nico Lance Medina <nmedina@tabletbasedtesting.com>
2016-06-15 15:58:50 +0800

    Add placeholder images, bottom content, enable faker fixture

--- views/site/index.php
+++ views/site/index.php
@@ -4,6 +4,7 @@
 
 $this->title = 'Foodzilla';
 ?>
+
 <div class="site-index">
 
     <div class="jumbotron">
@@ -12,9 +13,9 @@ $this->title = 'Foodzilla';
     </div>
 
     <div class="body-content">
-
         <div class="row">
             <div class="col-lg-4">
+                <img src="images/food-01.jpg" class="img-circle img-responsive center-block">
                 <h2>Supporting your Business</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -25,6 +26,7 @@ $this->title = 'Foodzilla';
                 <p><a class="btn btn-default" href="#">Learn More</a></p>
             </div>
             <div class="col-lg-4">
+                <img src="images/food-02.jpg" class="img-circle img-responsive center-block">
                 <h2>Providing YOU the Power</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -35,6 +37,7 @@ $this->title = 'Foodzilla';
                 <p><a class="btn btn-default" href="#">Learn More</a></p>
             </div>
             <div class="col-lg-4">
+                <img src="images/food-03.jpg" class="img-circle img-responsive center-block">
                 <h2>Full Control</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -43,8 +46,18 @@ $this->title = 'Foodzilla';
                     fugiat nulla pariatur.</p>
 
                 <p><a class="btn btn-default" href="#">Learn More</a></p>
+                <br>
+                <br>
             </div>
         </div>
-
+        <div class="row">
+            <div class="col-lg-5">
+                <img src="images/food-04.jpg" class="img-responsive center-block">
+            </div>
+            <div class="col-lg-7">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+        </div>
+        </div>
     </div>
 </div>
