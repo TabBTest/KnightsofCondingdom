648bd0c30c3dbaea2ce4a2897519be5564a51142
Nico Lance Medina <nmedina@tabletbasedtesting.com>
2016-06-17 14:58:18 +0800

    Vendor settings - disable redirect URL text field if checkbox is unchecked

--- views/vendor/settings.php
+++ views/vendor/settings.php
@@ -1,10 +1,23 @@
-<?php 
+<?php
 
 use app\models\VendorMenuItem;
 use app\models\TenantInfo;
 use app\helpers\TenantHelper;
 $this->title = 'Profile Settings';
 $this->params['breadcrumbs'][] = $this->title;
+
+$js = <<<'JS'
+if (!$("input[name='TenantCode[SUBDOMAIN_REDIRECT]']").is(":checked")) {
+    console.log('kamote');
+    $("input[name='TenantCode[REDIRECT_URL]']").prop("disabled",true);
+}
+
+$("input[name='TenantCode[SUBDOMAIN_REDIRECT]']").click(function() {
+    $("input[name='TenantCode[REDIRECT_URL]']").prop("disabled", !this.checked)
+});
+JS;
+
+$this->registerJs($js, $this::POS_READY);
 ?>
 
 <?php if(\Yii::$app->getSession()->hasFlash('error')){?>
@@ -38,27 +51,27 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class='col-xs-12'>
             <label class='form-label'>Your Widget</label>
             <textarea class='form-control' rows="5" cols="20">
-<script type="text/javascript" id="foodapp-js" src="//<?php echo \Yii::$app->params['defaultSiteURL']?>/js/sdk.js"></script> 
+<script type="text/javascript" id="foodapp-js" src="//<?php echo \Yii::$app->params['defaultSiteURL']?>/js/sdk.js"></script>
 <div class='foodzilla-widget-button' style='position: absolute; top: 10px; right: 10px; height: 20px; width: 100px; -moz-user-select: none;
     background-image: none;
     border: 1px solid transparent;
     border-radius: 4px;
-    font-size: 14px; 
+    font-size: 14px;
     padding: 6px 12px;
     text-align: center;background-color: #5cb85c; '>Show Menu
 </div>
 <div class="foodzilla-widget" data-subdomain='<?php echo TenantHelper::getVendorSubdomain(\Yii::$app->user->id)?>' style='position: absolute; right: 10px; top: 50px; display: none'></div></textarea>
         </div>
     </div>
-    
+
 <form action='/vendor/save-settings' method='POST'>
-    <?php 
+    <?php
     $userId = \Yii::$app->user->id;
     ?>
-<?php foreach(TenantInfo::getTenantCodes() as $codeKey => $codeDescription){       
+<?php foreach(TenantInfo::getTenantCodes() as $codeKey => $codeDescription){
     ?>
     <?php if($codeKey == TenantInfo::CODE_SUBDOMAIN_REDIRECT){?>
-    
+
     <div class='row form-group'>
         <div class='col-xs-12'>
             <label class='form-label'><?php echo $codeDescription?></label>
@@ -75,7 +88,7 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
     <?php }?>
   <?php }?>
-  
+
     <div class='row form-group'>
         <div class='col-xs-12'>
             <button class='btn btn-success'>Save</button>
