cddfdfdfe6d647e2a9a767d2fbd3deb7d9f5040a
Daniel Jao <daniel_jao@yahoo.com>
2016-07-14 19:54:09 +0800

    fix: Add link on payment issue notification to update payment info

--- views/layouts/_card_warning.php
+++ views/layouts/_card_warning.php
@@ -2,18 +2,21 @@
 if(!Yii::$app->user->isGuest){
         //Yii::$app->user->identity->cardExpiry = '2015-06-26';
         $cardState = Yii::$app->user->identity->getCardState();
+        $url = '/vendor/settings?view=billing';
+        if(Yii::$app->user->identity->role == User::ROLE_CUSTOMER)
+            $url = '/my/profile?view=billing';
         
         if($cardState == User::CARD_STATE_EXPIRED){
             ?>
-            <div class='alert alert-danger'>Card Information is expired</div>
+            <div class='alert alert-danger'>Card Information is expired, update your card <a href="<?php echo $url?>/">here</a>.</div>
         <?php
         }else if($cardState == User::CARD_STATE_NEAR_EXPIRE){
             ?>
-            <div class='alert alert-warning'>Your Card would expire in <?php echo date('M Y', strtotime(Yii::$app->user->identity->cardExpiry))?></div>
+            <div class='alert alert-warning'>Your Card would expire in <?php echo date('M Y', strtotime(Yii::$app->user->identity->cardExpiry))?>, update your card <a href="<?php echo $url?>/">here</a>.</div>
             <?php            
         }else if($cardState == User::CARD_STATE_NOT_EXISTING){
             ?>
-            <div class='alert alert-danger'>Please add your card billing information</div>
+            <div class='alert alert-danger'>Please add your card billing information, update your card <a href="<?php echo $url?>/">here</a>.</div>
             <?php
         }
         
